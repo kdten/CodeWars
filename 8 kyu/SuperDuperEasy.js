4d9d7f9de0d089b9f30b18c53f7fec0d7f2523f9
kdten <kolten.rasmussen@gmail.com>
2022-10-31 22:30:36 -0600

    Added 8 kyu

---
+++ 8 kyu/SuperDuperEasy.js
@@ -0,0 +1,20 @@
+//Super Duper Easy
+
+
+// Instructions
+
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+// My Solution
+
+function problem(x){
+    if (typeof(x) == 'string') {
+      return "Error"
+    } else {
+      return x*50+6
+    }
+  }
+
+// Best Practice
+
+const problem = x => typeof x === 'string' ? 'Error' : x * 50 + 6;
