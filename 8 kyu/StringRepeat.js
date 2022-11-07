00468afb95cd3d823d3074d606aab3a2aea070bb
kdten <kolten.rasmussen@gmail.com>
2022-11-07 10:23:53 -0700

    Added 8 kyu

---
+++ 8 kyu/StringRepeat.js
@@ -0,0 +1,29 @@
+// String repeat
+
+
+// Instructions
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+
+// My Solution
+
+const repeatStr=(n, s)=> {
+    let rStr = '';
+    for (let i=0;i<n;i++) {
+      rStr += s;
+    }
+    return rStr;
+  }
+
+// Best Practice
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
+
+repeatStr = (n, s) => s.repeat(n)
