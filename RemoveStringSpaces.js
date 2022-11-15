7df577d1831904067e443f07cc90a051a9ce4a93
Kolten Edward <102930886+kdten@users.noreply.github.com>
2022-11-15 10:08:46 -0700

    First Kata upload

---
+++ RemoveStringSpaces.js
@@ -0,0 +1,17 @@
+//Remove String Spaces
+
+//Instructions
+
+//Simple, remove the spaces from the string, then return the resultant string.
+
+//My Solution
+
+function noSpace(x){
+  return x.split(' ').join('')
+}
+
+//Best Practice
+
+function noSpace(x){
+  return x.replace(/\s/g, '');
+}
