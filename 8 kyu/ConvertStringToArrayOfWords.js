9e416fe92c2d224d2b51443cb35029d7535997ce
kdten <kolten.rasmussen@gmail.com>
2022-11-15 21:16:49 -0700

    8 kyu added

---
+++ 8 kyu/ConvertStringToArrayOfWords.js
@@ -0,0 +1,22 @@
+//Convert a string to an array
+
+//Instructions
+
+//Write a function to split a string and convert it into an array of words.
+
+
+//My Solution
+
+function stringToArray(string){
+    return string.trim().split(" ");
+}
+  
+  //Best Practice
+  
+function stringToArray(string){
+    return string.split(' ');
+}
+
+const stringToArray = string => string.split(' ');
+
+stringToArray=(s)=>s.split(' ')
