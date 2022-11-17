41831fd2d5bf68e54a56701e7f7419e10e1c8ce4
kdten <kolten.rasmussen@gmail.com>
2022-11-16 17:18:13 -0700

    8 kyu added

---
+++ 8 kyu/MakeUpperCase.js
@@ -0,0 +1,18 @@
+//MakeUpperCase
+
+//Instructions
+
+//Write a function which converts the input string to uppercase.
+
+
+//My Solution
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+  
+//Best Practice
+  
+const makeUpperCase = str => str.toUpperCase();
+
+  
