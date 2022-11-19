84462f125e0eb37488cf4936a6071c00aa91c040
kdten <kolten.rasmussen@gmail.com>
2022-11-19 09:55:29 -0700

    7 kyu folder and problem add

---
+++ 7 kyu/StringEndsWith.js
@@ -0,0 +1,29 @@
+// String ends with?
+
+
+// Instructions
+
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+
+// My Solution
+
+function solution(str, ending){
+    return str.endsWith(ending);
+  }
+
+// Best Practice
+
+function solution(str, ending){
+    return str.endsWith(ending);
+  }
+
+const solution = (str, ending) => str.endsWith(ending);
