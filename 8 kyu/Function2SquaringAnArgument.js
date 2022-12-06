79b422622b61d24c949efd4c26c2c06a5c6a8bdd
kdten <kolten.rasmussen@gmail.com>
2022-12-05 20:40:04 -0700

    Adds 8 kyu

---
+++ 8 kyu/Function2SquaringAnArgument.js
@@ -0,0 +1,19 @@
+// Function 2 - squaring an argument
+
+
+// Instructions
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// Write the "square"-function here
+
+
+// My Solution
+
+square = (x) => x * x
+
+// Best Practice
+
+function square(x) {
+    return x ** 2;
+  }
