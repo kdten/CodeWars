dec6e90707eb0991e3c790a0f49d271b382945e4
kdten <kolten.rasmussen@gmail.com>
2022-12-05 20:33:42 -0700

    Adds 8 kyu

---
+++ 8 kyu/MultiplyDebug.js
@@ -0,0 +1,21 @@
+// Multiply
+
+
+// Instructions
+
+// This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//     a * b
+//   }
+
+
+// My Solution
+
+function multiply(a, b){
+    return a * b
+  }
+
+  // Best Practice
+    
+  multiply = (a, b) => a * b;
