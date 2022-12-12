4b7609c7f25eb9ee4d461c0d50ff95f885096a23
kdten <kolten.rasmussen@gmail.com>
2022-12-11 21:33:23 -0700

    Adds 8 kyu

---
+++ 8 kyu/YouOnlyNeedOneBeginner.js
@@ -0,0 +1,17 @@
+// You only need one - Beginner
+
+// Instructions
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+// My Solution
+
+check = (a, x) => a.includes(x);
+
+// Best Practice
+  
+const check = (a,x) => a.includes(x);
