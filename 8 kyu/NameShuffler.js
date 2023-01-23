139b2bd67e56e29ffa3e47396c9d1cd6e5f709f7
kdten <kolten.rasmussen@gmail.com>
2023-01-22 19:44:36 -0700

    Adds 8 kyu

---
+++ 8 kyu/NameShuffler.js
@@ -0,0 +1,21 @@
+// Name Shuffler
+
+
+// Instructions
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// Example(Input --> Output)
+
+// "john McClane" --> "McClane john"
+
+
+// My Solution
+
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+  }
+
+// Best Practice
+
+const nameShuffler = str => str.split(' ').reverse().join(' ');
