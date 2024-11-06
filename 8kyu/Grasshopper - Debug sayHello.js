58ff969f44cbb25ce48614b00aa0043675c132df
kdten <kolten.rasmussen@gmail.com>
2024-11-06 14:15:42 -0700

    adds 8 kyu

---
+++ 8kyu/Grasshopper - Debug sayHello.js
@@ -0,0 +1,23 @@
+// // Grasshopper - Debug sayHello
+
+
+// // Instructions
+
+// // Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+
+// // My Solution
+
+// //function sayHello(name) {
+//     return `Hello, ${name}`
+// }
+
+// // Best Practice
+
+
+// //const sayHello = name => `Hello, ${name}`;
